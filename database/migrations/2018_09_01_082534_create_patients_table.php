98f1de786aaab029a8488b62c06f3cd4ead37796
elbardeny <mostafa.elbardeny@gmail.com>
2018-09-01 10:32:17 +0200

    added patients migration

---
+++ database/migrations/2018_09_01_082534_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('first_name')->index();
+
+            $table->string('second_name')->index();
+
+            $table->string('family_name')->index();
+
+            $table->integer('uid')->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
